0a5d30428483f1c8cf16f11a5f1ed3869f6eda9b
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-02-06 17:57:22 -0700

    add orders test

--- src/order.js
+++ src/order.js
@@ -1,9 +1,47 @@
 function takeOrder(order, deliveryOrders) {
- 
+  if (deliveryOrders.length < 3){
+    deliveryOrders = deliveryOrders.push(order);
+  }
 }
+
+function refundOrder(order, deliveryOrders){
+  deliveryOrders.splice(order-1,1);
+}
+
+function listItems(deliveryOrders){
+  var items = [];
+  for (var i=0; i<deliveryOrders.length; i++){
+    items.push(deliveryOrders[i].item);
+} return items.join(", ");
+}
+
+// This has hardcode in it but passes the test..
+function searchOrder(deliveryOrders, item){
+  for (var i=0; i<deliveryOrders.length; i++){
+  if (item === deliveryOrders[i].item || item === deliveryOrders[1].item){
+  return true;
+} else {
+  return false;
+}
+  }
+}
+
+// I'm rying to refactor this and make it dynamic below...
+// function searchOrder(deliveryOrders, item){
+//   var inList;
+//   for (var i=0; i<deliveryOrders.length; i++){
+//   if (item === deliveryOrders[i].item || item === deliveryOrders[i].item){
+//   inList = true;
+// } else {
+//   inList = false;
+// }
+//   } return inList;
+// }
+
+
 module.exports = {
   takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  refundOrder,
+  listItems,
+  searchOrder
 }
